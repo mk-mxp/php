6435df0b55d3ecb39c482f5bcba8442cd8ea8754
Michael Kramer <m.kramer@mxp.de>
2024-03-23 13:38:51 +0100

    Copy Group to InnerGroup
    This will remain what is made from the "cases" array itself

---
+++ contribution/generator/src/TrackData/InnerGroup.php
@@ -0,0 +1,46 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\TrackData;
+
+/**
+ * Represents a list of 'cases'
+ */
+class InnerGroup
+{
+    /**
+     * PHP_EOL is CRLF on Windows, we always want LF
+     * @see https://www.php.net/manual/en/reserved.constants.php#constant.php-eol
+     */
+    private const LF = "\n";
+
+    private function __construct(
+        private array $cases,
+    ) {
+    }
+
+    public static function from(mixed $rawData): ?self
+    {
+        if (!\is_array($rawData))
+            return null;
+
+        $testCases = [];
+        foreach($rawData as $rawCase) {
+            $thisCase = TestCase::from($rawCase);
+            if ($thisCase === null)
+                $thisCase = Unknown::from($rawCase);
+            $testCases[] = $thisCase;
+        }
+
+        return new static($testCases);
+    }
+
+    public function renderPhpCode(): string
+    {
+        return \implode(self::LF, \array_map(
+            fn ($case): string => $case->renderPhpCode(),
+            $this->cases,
+        ));
+    }
+}
