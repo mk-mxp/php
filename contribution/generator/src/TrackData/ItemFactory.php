5e39e7a92979c64cf66438534e172917a9528abd
Michael Kramer <m.kramer@mxp.de>
2024-03-25 14:29:20 +0100

    ItemFactory produces CanonicalData

--- contribution/generator/src/TrackData/ItemFactory.php
+++ contribution/generator/src/TrackData/ItemFactory.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace App\TrackData;
 
+use App\TrackData\CanonicalData;
 use App\TrackData\Item;
 use App\TrackData\Unknown;
 
@@ -14,11 +15,12 @@ class ItemFactory
 {
     public function from(mixed $rawData): Item
     {
+        $case = CanonicalData::from($rawData);
         // $case = TestCase::from($rawData);
         // if ($case === null)
         //     $case = Group::from($rawData);
-        // if ($case === null)
-        $case = Unknown::from($rawData);
+        if ($case === null)
+            $case = Unknown::from($rawData);
         return $case;
     }
 }
--- contribution/generator/tests/TestGeneration/ItemFactory/ItemFactoryTest.php
+++ contribution/generator/tests/TestGeneration/ItemFactory/ItemFactoryTest.php
@@ -3,9 +3,11 @@
 namespace App\Tests\TestGeneration\Group;
 
 use App\Tests\TestGeneration\ScenarioFixture;
+use App\TrackData\CanonicalData;
 use App\TrackData\ItemFactory;
 use App\TrackData\Item;
 use App\TrackData\Unknown;
+use PHPUnit\Framework\Attributes\DataProvider;
 use PHPUnit\Framework\Attributes\Test;
 use PHPUnit\Framework\Attributes\TestDox;
 use PHPUnit\Framework\TestCase as PHPUnitTestCase;
@@ -16,15 +18,30 @@ final class ItemFactoryTest extends PHPUnitTestCase
     use ScenarioFixture;
 
     #[Test]
-    public function detectsUnknown(): void
-    {
-        $scenario = 'empty-object';
+    #[TestDox('$_dataName')]
+    #[DataProvider('scenarios')]
+    public function detectsExpectedItemType(
+        string $scenario,
+        string $fqcn,
+    ): void {
         $input = $this->rawDataFor($scenario);
 
         $subject = new ItemFactory();
         $actual = $subject->from($input);
 
         $this->assertInstanceOf(Item::class, $actual);
-        $this->assertInstanceOf(Unknown::class, $actual);
+        $this->assertInstanceOf($fqcn, $actual);
+    }
+
+    public static function scenarios(): array
+    {
+        return [
+            'When given a minimal valid canonical data object, then produces CanonicalData'
+                => [ 'canonical-data-object-minimal', CanonicalData::class ],
+            'When given a maximal valid canonical data object, then produces CanonicalData'
+                => [ 'canonical-data-object-maximal', CanonicalData::class ],
+            'When given an empty object, then produces Unknown'
+                => [ 'empty-object', Unknown::class ],
+        ];
     }
 }
