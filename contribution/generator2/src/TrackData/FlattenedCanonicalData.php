900fe7f84b395d2cf2f49a293ef4aab24496dde0
Michael Kramer <m.kramer@mxp.de>
2025-10-12 19:21:16 +0200

    Test drive empty and single case scenarios
    Reusing previous things from rewrite lead here, but it's not much.

---
+++ contribution/generator2/src/TrackData/FlattenedCanonicalData.php
@@ -0,0 +1,53 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\TrackData;
+
+class FlattenedCanonicalData
+{
+    public function __construct(
+        public string $testClassName,
+        public string $solutionFileName,
+        public string $solutionClassName,
+        public array $cases,
+        public string $exercise = '',
+        public array $comments = [],
+        public object|null $unknown = null,
+    ) {}
+
+    public static function from(mixed $rawData): ?static
+    {
+        if (!\is_object($rawData))
+            return null;
+
+        $requiredProperties = [
+            'testClassName',
+            'solutionFileName',
+            'solutionClassName',
+            'cases',
+        ];
+        $actualProperties = \array_keys(\get_object_vars($rawData));
+        $requiredData = [];
+        foreach ($requiredProperties as $requiredProperty) {
+            if (!\in_array($requiredProperty, $actualProperties)) {
+                return null;
+            }
+            $requiredData[$requiredProperty] = $rawData->{$requiredProperty};
+            unset($rawData->{$requiredProperty});
+        }
+
+        $comments = $rawData->comments ?? [];
+        unset($rawData->comments);
+
+        $exercise = $rawData->exercise ?? '';
+        unset($rawData->exercise);
+
+        return new static(
+            ...$requiredData,
+            exercise: $exercise,
+            comments: $comments,
+            unknown: empty(\get_object_vars($rawData)) ? null : $rawData,
+        );
+    }
+}
---
+++ contribution/generator2/tests/UpdateTests/FlattenedCanonicalDataTest.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Tests\UpdateTests;
+
+use App\Tests\UpdateTests\ScenarioFixture;
+use App\TrackData\FlattenedCanonicalData;
+use PHPUnit\Framework\Attributes\DataProvider;
+use PHPUnit\Framework\Attributes\Test;
+use PHPUnit\Framework\Attributes\TestDox;
+use PHPUnit\Framework\TestCase;
+
+/**
+ * The problem specification has no `testClassName`, `solutionFileName`,
+ * `solutionClassName` fields, these are added by the exercise.
+ */
+#[TestDox('Flattened Canonical Data (App\Tests\UpdateTests)')]
+final class FlattenedCanonicalDataTest extends TestCase
+{
+    use ScenarioFixture;
+
+    #[Test]
+    #[TestDox('$_dataName')]
+    #[DataProvider('happyScenarios')]
+    public function testHappyScenario(
+        string $scenario,
+    ): void {
+        $subject = $this->subjectFor($scenario);
+
+        $this->assertJsonStringEqualsOutputFixture(
+            $scenario,
+            \json_encode($subject, \JSON_PRETTY_PRINT | \JSON_THROW_ON_ERROR),
+            $scenario,
+        );
+    }
+
+    public static function happyScenarios(): array
+    {
+        return [
+            'When given a valid object with all keys, then forwards empty cases array'
+                => [ 'empty-cases' ],
+            'When given a valid object with all keys, then forwards one case in array'
+                => [ 'one-case' ],
+        ];
+    }
+
+    private function subjectFor(string $scenario): ?FlattenedCanonicalData
+    {
+        return FlattenedCanonicalData::from($this->rawDataFor($scenario));
+    }
+}
