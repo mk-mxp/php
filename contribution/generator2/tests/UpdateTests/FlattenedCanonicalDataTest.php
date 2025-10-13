b4cd604f1e978d2d71f2f4e78ac6b93ccf5cfa92
Michael Kramer <m.kramer@mxp.de>
2025-10-13 13:10:53 +0200

    Throws on object with no `solutionFileName` key

--- contribution/generator2/tests/UpdateTests/FlattenedCanonicalDataTest.php
+++ contribution/generator2/tests/UpdateTests/FlattenedCanonicalDataTest.php
@@ -41,6 +41,8 @@ final class FlattenedCanonicalDataTest extends TestCase
                 => [ 'no-object', InvalidArgumentException::class ],
             'Throws when given an object with no `testClassName` key'
                 => [ 'no-test-class-name', TypeError::class ],
+            'Throws when given an object with no `solutionFileName` key'
+                => [ 'no-solution-file-name', TypeError::class ],
             'Throws when given an object with no `cases` key'
                 => [ 'no-cases', TypeError::class ],
         ];
