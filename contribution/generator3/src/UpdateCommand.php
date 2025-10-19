49c100be27c3964d773f0a077bd99d4cf7c8c6b5
Michael Kramer <m.kramer@mxp.de>
2025-10-19 11:46:28 +0200

    Pick and adjust Application from homersimpson
    - Adjust update to application
    - Fix PHP issues in update
    - Strip down to SingleCommandApplication adapter

---
+++ contribution/generator3/src/UpdateCommand.php
@@ -0,0 +1,51 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App;
+
+use Psr\Log\LogLevel;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Logger\ConsoleLogger;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\SingleCommandApplication;
+
+use function assert;
+use function is_string;
+
+class UpdateCommand extends SingleCommandApplication
+{
+    private const EXERCISES_PATH = '/exercises/practice/';
+
+    public function __construct()
+    {
+        parent::__construct('Exercism PHP Test Generator for Updates');
+    }
+
+    protected function configure(): void
+    {
+        parent::configure();
+
+        $this->setVersion('1.0.0');
+        $this->addArgument('project-dir', InputArgument::REQUIRED, 'Path to project with the exercises in ' . self::EXERCISES_PATH . '.');
+        $this->addArgument('exercise-slug', InputArgument::REQUIRED, 'Slug of the exercise in ' . self::EXERCISES_PATH . '.');
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output): int
+    {
+        $projectDir = $input->getArgument('project-dir');
+        $exerciseSlug = $input->getArgument('exercise-slug');
+        assert(is_string($projectDir), 'project-dir must be a string');
+        assert(is_string($exerciseSlug), 'exercise-slug must be a string');
+
+        $exercisePath = $projectDir . self::EXERCISES_PATH . $exerciseSlug;
+
+        $logger = new ConsoleLogger($output, [
+            LogLevel::NOTICE => OutputInterface::VERBOSITY_NORMAL,
+        ]);
+        $logger->notice('Updating exercise in path: ' . $exercisePath);
+
+        return self::SUCCESS;
+    }
+}
